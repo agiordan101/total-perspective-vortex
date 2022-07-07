c5b9e8e9d5511aa4f976bad5508589128c582063
antoine <agd611@gmail.com>
2022-07-07 16:29:02 +0200

    Preprocessing dataset

--- tests.py
+++ tests.py
@@ -11,27 +11,71 @@ import matplotlib.pyplot as plt
 
 
 DIR_NAME = os.path.dirname(__file__)
-FILES_NAME = ['S001R01.edf', 'S001R01.edf.event']
+EDF_FILES_DIR = "data"
 
 
-def fetch_data(files_name: list):
+def print_raws_properties(raws):
+    print(f"\n\nRaws infos:", raws)
+    print(raws.info)
+    print(raws.times)
+    print(len(raws.times))
+
+    print(f"\nAnnotations infos:")
+    print(raws.annotations)
+
+    events = mne.events_from_annotations(raws)
+    print(f"\nEvents infos:")
+    print(events)
+
+
+def fetch_data(verbose=False):
     """
-    Fetch data as raws from an edf file
+        Fetch data as raws from an edf file
     """
-    files = [os.path.join(DIR_NAME, "data", f) for f in files_name]
-    print(files)
-  
+    dir_path = os.path.join(DIR_NAME, EDF_FILES_DIR)
+    dir_content_path = [os.path.join(dir_path, f) for f in os.listdir(dir_path)]
+
     raws = []
-    for f in files:
-        raws.append(mne.io.read_raw_edf(f, preload=True))
+    for f in dir_content_path:
+        print(f"Obj: {f}", os.path.isfile(f), os.path.splitext(f)[1] == '.edf')
+        if os.path.isfile(f) and os.path.splitext(f)[1] == '.edf':
+            print(f"EDF file: {f}")
+            raws.append(mne.io.read_raw_edf(f, preload=True))
 
-    return mne.concatenate_raws(raws)
+    raws = mne.concatenate_raws(raws) if raws else None
 
+    if verbose:
+        print_raws_properties(raws)
+    return raws
 
-def preprocessing_data(raw):
-    """ Filters"""
-    pass
+def preprocessing_data(raws, verbose=False):
+    """
+        Filter data
 
+        Each annotation includes one of three codes (T0, T1, or T2):
+            T0 corresponds to rest
+            T1 corresponds to onset of motion (real or imagined) of
+                - the left fist (in runs 3, 4, 7, 8, 11, and 12)
+                - both fists (in runs 5, 6, 9, 10, 13, and 14)
+            T2 corresponds to onset of motion (real or imagined) of
+                - the right fist (in runs 3, 4, 7, 8, 11, and 12)
+                - both feet (in runs 5, 6, 9, 10, 13, and 14)
+
+        Transform annotations T1 for runs 5, 6, 9, 10, 13, and 14 to T3
+        Transform annotations T2 for runs 5, 6, 9, 10, 13, and 14 to T4
+
+        Goal:
+            T0: rest
+            T1: the left fist
+            T2: the right fist
+            T3: both fists
+            T4: both feet
+    """
+    raws = raws.crop(tmax=40)
+
+    if verbose:
+        print_raws_properties(raws)
+    return raws
 
 def transform_data(raw):
     """
@@ -41,29 +85,24 @@ def transform_data(raw):
     return raw
 
 
-def create_pipeline():
+# def create_pipeline():
 
-    cv = ShuffleSplit(10, test_size=0.2, random_state=42)
-    pipeline = Pipeline([
-        'PCA': 
-    ])
-
+#     cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+#     pipeline = Pipeline([
+#         'PCA': 
+#     ])
 
 
 if __name__ == "__main__":
+    """
+        Try to classify baseline rest and both fists first
+    """
 
-    raw = fetch_data(FILES_NAME)
-    events = mne.events_from_annotations(raw)
+    raws = fetch_data(verbose=True)
 
-    print(raw)
-    print(raw.info)
-    print(raw.info["ch_names"])
-    print(raw.annotations)
-    print(events)
+    raws = preprocessing_data(raws, verbose=True)
 
-    raw.plot_psd(fmax=80)
-    raw.plot(duration=5, n_channels=64, block=True)
+
+    # raw.plot_psd(fmax=80)
+    # raw.plot(duration=5, n_channels=64, block=True)
     # print(mne.sys_info())
-
-    # data = transform_data(raw)
-    # print(data)
